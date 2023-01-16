68111ff7ab1c990ffb9916fbe05e415edd544fbc
muhammhassan <muhammadhassan7788@gmail.com>
2023-01-16 12:53:48 +0700

    Add Home Component

---
+++ app/src/main/java/com/muhammhassan/newsapp/ui/ListFragment.kt
@@ -0,0 +1,26 @@
+package com.muhammhassan.newsapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muhammhassan.newsapp.R
+import com.muhammhassan.newsapp.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
