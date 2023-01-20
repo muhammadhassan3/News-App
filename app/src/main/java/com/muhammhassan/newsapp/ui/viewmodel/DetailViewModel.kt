2b29a6838edcf44a207b439ee5f2517afdf35ad9
muhammhassan <muhammadhassan7788@gmail.com>
2023-01-20 21:47:54 +0700

    Create view model class

---
+++ app/src/main/java/com/muhammhassan/newsapp/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,31 @@
+package com.muhammhassan.newsapp.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.muhammhassan.domain.model.NewsModel
+import com.muhammhassan.domain.usecase.DetailUseCase
+import kotlinx.coroutines.launch
+
+class DetailViewModel(val useCase: DetailUseCase) : ViewModel() {
+    private val _isBookmarked = MutableLiveData<Boolean>()
+    val isBookmarked: LiveData<Boolean> get() = _isBookmarked
+
+    fun getIsBookmarked(title: String): LiveData<Boolean> {
+        viewModelScope.launch {
+            useCase.getSpecifiedData(title).collect {
+                _isBookmarked.value = it
+            }
+        }
+        return isBookmarked
+    }
+
+    fun addBookmarkedData(data: NewsModel) {
+        useCase.addBookmarkedData(data)
+    }
+
+    fun removeBookmarkedData(data: NewsModel) {
+        useCase.removeBookmarkedData(data)
+    }
+}
