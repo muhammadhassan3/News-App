b62418e1db6472c8bbd7a50d457e9bd503e58cf0
muhammhassan <muhammadhassan7788@gmail.com>
2023-01-20 21:47:29 +0700

    Fix get specified data

--- core/src/main/java/com/muhammhassan/core/repository/NewsRepository.kt
+++ core/src/main/java/com/muhammhassan/core/repository/NewsRepository.kt
@@ -2,8 +2,14 @@ package com.muhammhassan.core.repository
 
 import com.muhammhassan.core.api.ApiResponse
 import com.muhammhassan.core.api.model.Articles
+import com.muhammhassan.core.database.entity.BookmarkEntity
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
     fun getNews(): Flow<ApiResponse<List<Articles>>>
+
+    fun addBookmarkNews(data: BookmarkEntity)
+    fun removeBookmarkNews(title: String)
+    fun getBookmarkedNews(): Flow<List<BookmarkEntity>>
+    fun getSpecifiedData(title: String): Flow<BookmarkEntity?>
 }
--- core/src/main/java/com/muhammhassan/core/repository/NewsRepositoryImpl.kt
+++ core/src/main/java/com/muhammhassan/core/repository/NewsRepositoryImpl.kt
@@ -2,20 +2,38 @@ package com.muhammhassan.core.repository
 
 import com.muhammhassan.core.api.ApiResponse
 import com.muhammhassan.core.api.model.Articles
+import com.muhammhassan.core.database.entity.BookmarkEntity
+import com.muhammhassan.core.datasource.LocalDataSource
 import com.muhammhassan.core.datasource.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 
-class NewsRepositoryImpl(private val remote: RemoteDataSource): NewsRepository {
+class NewsRepositoryImpl(private val remote: RemoteDataSource, private val local: LocalDataSource): NewsRepository {
     override fun getNews(): Flow<ApiResponse<List<Articles>>> {
         return remote.getNews()
+    }
+
+    override fun addBookmarkNews(data: BookmarkEntity) {
+        local.addData(data)
+    }
+
+    override fun removeBookmarkNews(title: String) {
+        local.deleteData(title)
+    }
+
+    override fun getBookmarkedNews(): Flow<List<BookmarkEntity>> {
+        return local.getData()
+    }
+
+    override fun getSpecifiedData(title: String): Flow<BookmarkEntity?> {
+        return local.getSpecifiedData(title)
     }
 
     companion object{
         @Volatile
         private var INSTANCE: NewsRepositoryImpl? = null
 
-        fun getInstance(remote: RemoteDataSource) = INSTANCE ?: synchronized(this){
-            val instance = NewsRepositoryImpl(remote)
+        fun getInstance(remote: RemoteDataSource, local: LocalDataSource) = INSTANCE ?: synchronized(this){
+            val instance = NewsRepositoryImpl(remote, local)
             INSTANCE = instance
             instance
         }
