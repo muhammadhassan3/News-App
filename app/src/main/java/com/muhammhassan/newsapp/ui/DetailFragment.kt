a72fb7ce90d702dccaac6fd92b0475858af90610
muhammhassan <muhammadhassan7788@gmail.com>
2023-01-20 20:24:09 +0700

    Add DetailFragment code

--- app/src/main/java/com/muhammhassan/newsapp/ui/DetailFragment.kt
+++ app/src/main/java/com/muhammhassan/newsapp/ui/DetailFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.muhammhassan.newsapp.R
 import com.muhammhassan.newsapp.databinding.FragmentDetailBinding
@@ -23,5 +26,18 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val data = DetailFragmentArgs.fromBundle(arguments as Bundle).item
+
+        binding.apply{
+            toolbar.apply{
+                title = data.title
+                inflateMenu(R.menu.detail_menu)
+                toolbar.menu.getItem(0).icon = ContextCompat.getDrawable(requireContext(), R.drawable.baseline_bookmark_24)
+                toolbar.menu.getItem(0).setOnMenuItemClickListener {
+                    Toast.makeText(requireContext(), "Test", Toast.LENGTH_SHORT).show()
+                    true
+                }
+            }
+
+        }
     }
 }
