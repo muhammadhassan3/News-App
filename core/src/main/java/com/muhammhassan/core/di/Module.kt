6bf3d5b69dc7fa3b327d4a8c889193fc33f8ecd7
muhammhassan <muhammadhassan7788@gmail.com>
2023-01-28 20:19:27 +0700

    Add database security

--- core/src/main/java/com/muhammhassan/core/di/Module.kt
+++ core/src/main/java/com/muhammhassan/core/di/Module.kt
@@ -2,6 +2,7 @@ package com.muhammhassan.core.di
 
 import android.content.Context
 import androidx.room.Room
+import com.muhammhassan.core.BuildConfig
 import com.muhammhassan.core.api.ApiClient
 import com.muhammhassan.core.api.ApiClient.Companion.getNewsService
 import com.muhammhassan.core.database.NewsDatabase
@@ -11,6 +12,8 @@ import com.muhammhassan.core.datasource.RemoteDataSource
 import com.muhammhassan.core.datasource.RemoteDataSourceImpl
 import com.muhammhassan.core.repository.NewsRepository
 import com.muhammhassan.core.repository.NewsRepositoryImpl
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -29,10 +32,14 @@ object Module {
     }
 
     val provideDatabase = module {
-        fun dbInstance(context: Context) =
-            Room.databaseBuilder(context, NewsDatabase::class.java, "news_db")
+        fun dbInstance(context: Context): NewsDatabase {
+            val password = SQLiteDatabase.getBytes(BuildConfig.DB_PASS.toCharArray())
+            val factory = SupportFactory(password)
+            return Room.databaseBuilder(context, NewsDatabase::class.java, "news_db")
                 .fallbackToDestructiveMigration()
+                .openHelperFactory(factory)
                 .build()
+        }
         single { dbInstance(androidContext()) }
     }
 }
