1cc54b8c9dd61948579273242c47ab28bfd98285
muhammhassan <muhammadhassan7788@gmail.com>
2023-01-28 20:20:35 +0700

    Add Certificate pinner

--- core/src/main/java/com/muhammhassan/core/api/ApiClient.kt
+++ core/src/main/java/com/muhammhassan/core/api/ApiClient.kt
@@ -1,5 +1,6 @@
 package com.muhammhassan.core.api
 
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -7,11 +8,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiClient {
     companion object {
+        private const val hostname = "newsapi.org"
+        private val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/7xmA6N1F1gp6ikj57Bg4DMG0jfUB+mZsEL4mZO0qbfU=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
+
         private val client = OkHttpClient.Builder().addInterceptor(
             HttpLoggingInterceptor().setLevel(
                 HttpLoggingInterceptor.Level.BODY
             )
-        ).addInterceptor(HeaderAuthorization()).build()
+        ).addInterceptor(HeaderAuthorization()).certificatePinner(certificatePinner).build()
 
         @Volatile
         private var INSTANCE: Retrofit? = null
@@ -27,7 +35,7 @@ class ApiClient {
                 instance
             }
 
-        fun Retrofit.getNewsService(): ApiInterface{
+        fun Retrofit.getNewsService(): ApiInterface {
             return this.create(ApiInterface::class.java)
         }
     }
